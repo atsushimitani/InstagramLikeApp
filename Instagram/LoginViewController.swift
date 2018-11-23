d038d083e42e5590448b22136c1b42a9b5b6477d
Atsushi Mitani <tech.mitani@gmail.com>
2018-11-23 21:46:07 +0900

    アカウント作成処理

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -22,6 +22,42 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func handleCreateAccountButton(_ sender: Any) {
+        if let address = mailAddressTextField.text, let password = passwordTextField.text, let displayName = displayNameTextField.text {
+            
+            // アドレスとパスワードと表示名のいずれかでも入力されていない時は何もしない
+            if address.isEmpty || password.isEmpty || displayName.isEmpty {
+                print("DEBUG_PRINT: 何かが空文字です。")
+                return
+            }
+            
+            // アドレスとパスワードでユーザー作成。ユーザー作成に成功すると、自動的にログインする
+            Auth.auth().createUser(withEmail: address, password: password) { user, error in
+                if let error = error {
+                    // エラーがあったら原因をprintして、returnすることで以降の処理を実行せずに処理を終了する
+                    print("DEBUG_PRINT: " + error.localizedDescription)
+                    return
+                }
+                print("DEBUG_PRINT: ユーザー作成に成功しました。")
+                
+                // 表示名を設定する
+                let user = Auth.auth().currentUser
+                if let user = user {
+                    let changeRequest = user.createProfileChangeRequest()
+                    changeRequest.displayName = displayName
+                    changeRequest.commitChanges { error in
+                        if let error = error {
+                            // プロフィールの更新でエラーが発生
+                            print("DEBUG_PRINT: " + error.localizedDescription)
+                            return
+                        }
+                        print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました。")
+                        
+                        // 画面を閉じてViewControllerに戻る
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
+        }
     }
     
     override func viewDidLoad() {
