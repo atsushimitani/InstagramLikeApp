eb1dc642d8cc55e6b994e35dea829b52d6554da8
Atsushi Mitani <tech.mitani@gmail.com>
2018-11-24 10:49:33 +0900

    写真投稿画面

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 三谷淳史 on 2018/11/24.
+//  Copyright © 2018年 atsushi.mitani. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
